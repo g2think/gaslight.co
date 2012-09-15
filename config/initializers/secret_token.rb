e7a1897dae00581db77f840d370502427d04b1a9
Chris Moore <cdmwebs@gmail.com>
2012-09-14 21:47:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaslightNew::Application.config.secret_token = 'c00a2cbc9637c17db4693774e01a2c69c5e7e91153bf0ae83452a13e75d1317cf8dd3bdfe45e122ec3db72455085c1844f680ea1e939c94d19c08e6defe8bf2d'
