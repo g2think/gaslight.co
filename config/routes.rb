9a63a081c87a0f9bf39aa2d94345d2330fd5b2f8
Kevin Rockwood <kevin@rockwood.me>
2012-09-29 23:54:30 -0400

    Remove HighVoltage catchall route. It was conflicting with jasminerice

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Gaslight::Application.routes.draw do
   resource :contact, controller: :contact, only: :create
 
   # Look for a High Voltage page
-  match "/:id", to: 'pages#show', as: :static
+  # match "/:id", to: 'pages#show', as: :static
 
   root to: 'pages#show', id: 'home'
 end
