f3b4c027eea44b6d40f70609b6b3807dca238f66
Chris Moore <cdmwebs@gmail.com>
2012-10-16 22:27:06 -0400

    render the layout on 404

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,7 +19,7 @@ class PagesController < HighVoltage::PagesController
 
   rescue_from ActionView::MissingTemplate do |exception|
     if exception.message =~ %r{Missing template #{content_path}}
-      render action: :home, status: 404, layout: false
+      render action: :home, status: 404
     else
       raise exception
     end
