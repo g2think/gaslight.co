f52331f7c39bdb0432544e39dbf5650a249a252e
Chris Moore <cdmwebs@gmail.com>
2012-10-28 20:09:55 -0400

    remove airbrake

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key     = ENV.fetch('ERRBIT_API_KEY')
-  config.host        = ENV.fetch('ERRBIT_HOST')
-  config.port        = 80
-  config.secure      = config.port == 443
-end
