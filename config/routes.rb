08f28ca5d894bdafd478899e81e137f70ff069f5
Kristin Lasita <kristin@gaslight.co>
2012-12-19 11:44:24 -0500

    make page urls top level

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,5 @@ Gaslight::Application.routes.draw do
 
   # catch all the pages
   match '/:id', to: 'pages#show', as: :static
-  match '/pages/:id', to: 'pages#show', as: :static
   match '/', to: 'pages#show', id: 'home', as: :home
-
-  # redirect to /home. Lets the app rewrite requests
-  # and caches the page(s).
-  root to: 'pages#show', id: 'home', as: :home
 end
