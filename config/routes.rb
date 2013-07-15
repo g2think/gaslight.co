f6c91733544e7bef572a129eadf87142760a99f0
Chris Moore <cdmwebs@gmail.com>
2013-07-15 15:34:54 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,16 @@ Gaslight::Application.routes.draw do
 
   resource :contact, controller: :contact, only: :create
 
-  match "/blog/:year(/:month(/:day))" => "posts#index", constraints: { year: /\d{4}/, month: /\d{2}/, day: /\d{2}/ }
-  get 'blog/:slug', to: 'posts#show'
-  get 'post/:id(/:slug)', to: 'posts#show' # handle old tumblr urls
   resources :posts, path: '/blog', except: :show do
     collection do
+      get "/:year(/:month(/:day))" => "posts#index", constraints: { year: /\d{4}/, month: /\d{2}/, day: /\d{2}/ }
       get :recent
       get :search
     end
   end
+
+  get 'blog/:slug', to: 'posts#show'
+  get 'post/:id(/:slug)', to: 'posts#show' # handle old tumblr urls
 
   # catch all the pages
   match '/:id', to: 'pages#show', as: :static
