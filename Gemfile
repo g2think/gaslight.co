2506b3c7f3d922081d540229170aeb9263669b50
Chris Moore <cdmwebs@gmail.com>
2013-07-15 23:34:09 -0400

    adios cucumber

--- Gemfile
+++ Gemfile
@@ -42,7 +42,6 @@ group :development, :test do
 end
 
 group :test do
-  gem 'cucumber-rails', '1.3.0', :require => false
   gem 'poltergeist'
   gem 'factory_girl_rails'
   gem 'timecop'
--- features/support/javascript.rb
+++
@@ -1,8 +0,0 @@
-require 'capybara/poltergeist'
-
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-# Can also use :chrome
-Capybara.javascript_driver = :poltergeist
