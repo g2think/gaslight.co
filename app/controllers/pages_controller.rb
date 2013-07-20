af684516d53f344fddf9e83606393e8b7f99b978
Chris Moore <cdmwebs@gmail.com>
2013-07-20 14:54:58 -0400

    render an actual 404 page with some places to go.
    * needs design love.
    * needs background image.
    
    @kclasita, @tammygambrel, @arthurra, @heflinao
    
    for #41

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < HighVoltage::PagesController
+  expose(:posts) { Post.published.by_publish_date.limit(4) }
+  expose(:podcasts) { Post.published.tagged('podcast').by_publish_date.limit(4) }
+
   protected
 
   # FIXME: Where does this belong?
@@ -7,7 +10,7 @@ class PagesController < HighVoltage::PagesController
 
   rescue_from ActionView::MissingTemplate do |exception|
     if exception.message =~ %r{Missing template #{page_finder.content_path}}
-      render template: 'pages/home', formats: [:html], status: 404
+      render template: 'pages/404', formats: [:html], status: 404
     else
       raise exception
     end
